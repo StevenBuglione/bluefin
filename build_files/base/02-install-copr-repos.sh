ce46ec6d4a6d81eb1e8896ab30ef23f91f271ef5
StevenBuglione <stevenbuglione1@gmail.com>
2024-11-28 23:26:34 -0500

    update remove sudo

--- build_files/base/02-install-copr-repos.sh
+++ build_files/base/02-install-copr-repos.sh
@@ -16,7 +16,7 @@ curl --retry 3 -Lo /etc/yum.repos.d/_copr_che-nerd-fonts-"$(rpm -E %fedora)".rep
 
 # 1Password
 mkdir -p /var/opt
-curl https://downloads.1password.com/linux/keys/1password.asc | sudo tee /etc/pki/rpm-gpg/RPM-GPG-KEY-1password
+curl https://downloads.1password.com/linux/keys/1password.asc | tee /etc/pki/rpm-gpg/RPM-GPG-KEY-1password
 sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=0\ngpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-1password" > /etc/yum.repos.d/1password.repo'
 
 # Brave Browser
